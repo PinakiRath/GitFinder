2234b46aad5b106c95799d75304d30cd75eaa895
Pinaki Rath <pinakirath99@gmail.com>
2026-02-11 23:55:42 +0530

    refactor: clean up db.js and enhance comment clarity in githubHelper.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGODB_URI || '8000');
-
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
