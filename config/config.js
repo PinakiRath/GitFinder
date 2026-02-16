4e91a517a89ffa4a943a24c2aa947e105985ac95
Pinaki Rath <pinakirath99@gmail.com>
2026-02-16 23:41:45 +0530

    fix: remove semicolon from githubApi property and ensure newline at end of config.js

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 export default {
     port: process.env.PORT || 3000,
     githubToken: process.env.GITHUB_TOKEN,
-    githubApi: 'https://api.github.com';
+    githubApi: 'https://api.github.com'
 };
